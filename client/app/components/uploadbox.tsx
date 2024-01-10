b61fd86d8c4d817396dab0c2e438e840a5a0e04b
UkemeSkywalker <skywalkershadow9@gmail.com>
2024-01-10 14:14:27 +0100

    client dir

--- client/app/components/uploadbox.tsx
+++ client/app/components/uploadbox.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
-import { FaUpload } from 'react-icons/fa';
+import DragAndDrop from './dragAndDrop'
 
 function Uploadbox() {
   return (
     <div className='upload-box flex-auto w-1/4 bg-white p-10 flex flex-col items-center divide-x"'>
 
-            <div className='upload w-full flex flex-col p-20 rounded-lg justify-center  items-center bg-gray-100'>
-              <FaUpload className='text-4xl  text-blue-500' />
-              <p className='font-black text-blue-500'>Upload</p>
-            </div>
-
+            < DragAndDrop />
+            
             <div className='w-full flex flex-col mt-10 rounded-lg px-4 bg-gray-100 p-4'>
               <div className='flex flex-col'>
                 <div className='flex flex-row gap-2 justify-between'>            
